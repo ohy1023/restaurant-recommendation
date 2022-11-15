fc0c7a06030dbf1aba78d6bc129ab10687cd213c
ohy1023 <zvyg1023@naver.com>
2022-11-16 00:51:34 +0900

    음식 종류 입력 받으면 가까운 식당 id 반환 그 id로 리뷰 출력

--- content/tests.py
+++ content/tests.py
@@ -103,7 +103,6 @@ class OsmnxTestClass(TestCase):
 
         from tqdm import tqdm
 
-        # df2 = df[0:10000]
         df2.reset_index(drop=True, inplace=True)
         road_li = []  # 도로 기준 최단 거리
 
@@ -114,62 +113,8 @@ class OsmnxTestClass(TestCase):
                 orig_node = ox.nearest_nodes(Gs, X=user_y, Y=user_x)  # 출발지
                 dest_node = ox.nearest_nodes(Gs, X=df2['X'][i], Y=df2['Y'][i])  # 목적지
                 len_road = nx.shortest_path_length(Gs, orig_node, dest_node, weight='length')
-                road_li.append(str(round(len_road, 1)) + 'm')
+                road_li.append(len_road)
 
-        road_li3 = pd.DataFrame(road_li)
-        road_li3 = ['최단거리', '최단시간']
+        df2['minDist'] = road_li
 
-        t = []  # 최단거리 bike 기준 시간
-        bike_sp = 15000  # 자전거 속도 15km/h
-        road_li = pd.DataFrame(road_li)
-
-        road_li.columns = ['최단거리']
-        split = road_li['최단거리'].str.split('m')
-        road_li['최단거리'] = split.str.get(0)
-
-        for i in range(len(road_li)):
-            etc = float(road_li['최단거리'][i]) / bike_sp
-            hour = int(etc)
-            mint = int((etc - hour) * 60)
-            sec = int(((etc - hour) * 60 - mint) * 60)
-            if hour == 0:
-                t.append([mint, sec])
-            else:
-                t.append([hour, mint, sec])
-
-        road_li['최단시간'] = t
-
-        user_loc = pd.DataFrame({'위도': [user_x], '경도': [user_y]})
-        user_loc
-
-        df_diff = pd.DataFrame()
-        stores = []
-        diff = []
-        df_3 = pd.DataFrame()
-        df3_rank1_stores = []
-        df3_rank1_diff = []
-        df3_rank2_stores = []
-        df3_rank2_diff = []
-
-        for i in range(len(user_loc)):
-            for j in tqdm(range(len(df2))):
-                stores.append(df2['식당 이름'][j])
-                diff.append(np.sqrt((user_loc['위도'][i] - df2['Y'][j]) ** 2 +
-                                    (user_loc['경도'][i] - df2['X'][j]) ** 2))
-
-            df_diff['식당 이름'] = stores
-            df_diff['차이'] = diff
-            df_diff['순위'] = df_diff['차이'].rank(ascending=True)
-            df_diff.sort_values('순위', inplace=True)
-            df3_rank1_stores.append(df_diff.iloc[0]['식당 이름'])
-            df3_rank1_diff.append(df_diff.iloc[0]['차이'])
-            df3_rank2_stores.append(df_diff.iloc[1]['식당 이름'])
-            df3_rank2_diff.append(df_diff.iloc[1]['차이'])
-            stores = []
-            diff = []
-
-        user_loc['1순위'] = df3_rank1_stores
-        user_loc['1순위 차이'] = df3_rank1_diff
-        user_loc['2순위'] = df3_rank2_stores
-        user_loc['2순위 차이'] = df3_rank2_diff
-        print(user_loc)
+        print(df2.info())
--- content/views.py
+++ content/views.py
@@ -8,6 +8,7 @@ import requests
 from tqdm import tqdm
 import osmnx as ox, networkx as nx
 import pandas as pd
+import json
 
 
 # JsonResponse 출력 테스트용
@@ -65,78 +66,54 @@ def findNearRestaurant(request):
             orig_node = ox.nearest_nodes(Gs, X=user_y, Y=user_x)  # 출발지
             dest_node = ox.nearest_nodes(Gs, X=df2['X'][i], Y=df2['Y'][i])  # 목적지
             len_road = nx.shortest_path_length(Gs, orig_node, dest_node, weight='length')
-            road_li.append(str(round(len_road, 1)) + 'm')
+            road_li.append(len_road)
 
-    t = []  # 최단거리 bike 기준 시간
-    bike_sp = 3000  # 사람 속도 3km/h
-    road_li = pd.DataFrame(road_li)
+    df2['minDist'] = road_li
 
-    road_li.columns = ['최단거리']
-    split = road_li['최단거리'].str.split('m')
-    road_li['최단거리'] = split.str.get(0)
+    df2 = df2.sort_values(by='minDist', axis=0, ascending=True)
 
-    for i in range(len(road_li)):
-        etc = float(road_li['최단거리'][i]) / bike_sp
-        hour = int(etc)
-        mint = int((etc - hour) * 60)
-        sec = int(((etc - hour) * 60 - mint) * 60)
-        if hour == 0:
-            t.append([mint, sec])
-        else:
-            t.append([hour, mint, sec])
+    temp = list()
 
-    road_li['최단시간'] = t
+    for i in list(df2['pk'][:10]):
 
-    user_loc = pd.DataFrame({'위도': [user_x], '경도': [user_y]})
-    user_loc
+    # querySet2 = restaurant_review.objects.filter(restaurant_id=438979).select_related('restaurant_id')\
+    #     .prefetch_related('restaurant_id__restaurant_review_set')
 
-    df_diff = pd.DataFrame()
-    stores = []
-    diff = []
-    df3_rank1_stores = []
-    df3_rank1_diff = []
-    df3_rank2_stores = []
-    df3_rank2_diff = []
+        querySet2 = restaurant_info.objects.filter(id=i)
 
-    for i in range(len(user_loc)):
-        for j in tqdm(range(len(df2))):
-            stores.append(df2['pk'][j])
-            diff.append(np.sqrt((user_loc['위도'][i] - df2['Y'][j]) ** 2 +
-                                (user_loc['경도'][i] - df2['X'][j]) ** 2))
+        querySet3 = restaurant_review.objects.filter(restaurant_id=i).values_list('review', flat=True)
 
-        df_diff['식당 이름'] = stores
-        df_diff['차이'] = diff
-        df_diff['순위'] = df_diff['차이'].rank(ascending=True)
-        df_diff.sort_values('순위', inplace=True)
-        df3_rank1_stores.append(df_diff.iloc[0]['식당 이름'])
-        df3_rank1_diff.append(df_diff.iloc[0]['차이'])
-        df3_rank2_stores.append(df_diff.iloc[1]['식당 이름'])
-        df3_rank2_diff.append(df_diff.iloc[1]['차이'])
-        stores = []
-        diff = []
+        data = querySet2.get()
+        reviews = list(querySet3)
 
-    user_loc['1순위'] = df3_rank1_stores
-    user_loc['1순위 차이'] = df3_rank1_diff
-    user_loc['2순위'] = df3_rank2_stores
-    user_loc['2순위 차이'] = df3_rank2_diff
-    print(user_loc)
+        test = json.dumps({
+            'message': {
+                "restaurant_id": data.id,
+                "name": data.name,
+                "x": data.x,
+                "y": data.y,
+                "address": data.address,
+                "url": data.url,
+                'reviews': reviews
+            },
+            'keyboard': {
+                'type': 'text',
+            }},ensure_ascii=False)
 
-    querySet2 = restaurant_review.objects.filter(restaurant_id=user_loc['1순위'][0]).select_related(
-        'restaurant_id').prefetch_related('restaurant_id__restaurant_review_set')
+        temp.append(test)
+    print(temp)
 
-    result = [{
-        "restaurant_id": data.restaurant_id.id,
-        "name": data.restaurant_id.name,
-        "x": data.restaurant_id.x,
-        "y": data.restaurant_id.y,
-        "address": data.restaurant_id.address,
-        "url": data.restaurant_id.url,
-        "id": data.id,
-        "score": data.score,
-        "review": data.review
-    } for data in querySet2]
+
     return JsonResponse({
-        'message': result,
+        'message': {
+            "restaurant_id": data.id,
+            "name": data.name,
+            "x": data.x,
+            "y": data.y,
+            "address": data.address,
+            "url": data.url,
+            'reviews': reviews
+        },
         'keyboard': {
             'type': 'text',
         }
