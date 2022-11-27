f252f9c8c0f09fa8f50d21192296ce04f16df1b5
ohy1023 <zvyg1023@naver.com>
2022-11-27 21:12:50 +0900

    webdriver-manager 추가

--- service.py
+++ service.py
@@ -29,6 +29,8 @@ from tqdm import tqdm
 import osmnx as ox, networkx as nx
 import requests
 import MySQLdb
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -305,8 +307,9 @@ if __name__ == '__main__':
                                                                'protected_media_identifier': 2, 'app_banner': 2,
                                                                'site_engagement': 2, 'durable_storage': 2}}
                 options.add_experimental_option('prefs', prefs)
-
-                driver = webdriver.Chrome('D:/공주대학교/4-2/종합 설계/python_project/chromedriver.exe', options=options)
+                driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+                # driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),options=options)
+                # driver = webdriver.Chrome('D:/공주대학교/4-2/종합 설계/python_project/chromedriver.exe', options=options)
                 driver.maximize_window()
 
                 info = pd.DataFrame(columns=['종류', '별점', '리뷰 개수', '오픈 시간', '마감 시간', '해시 태그', '리뷰'])
